df1dc0ef956827aa44ad582c3210c07fbd77c1a7
GBergatto <gb.bergatto@gmail.com>
2025-08-05 18:00:45 +0200

    Define ALU op enum

---
+++ core/rtl/core_types_pkg.sv
@@ -0,0 +1,17 @@
+package core_types_pkg;
+
+   // Ordered following the R-type instructions in Table from Chapter 35
+   typedef enum logic [3:0] {
+    ALU_ADD,
+    ALU_SUB,
+    ALU_SLL,
+    ALU_SLT,
+    ALU_SLTU,
+    ALU_XOR,
+    ALU_SRL,
+    ALU_SRA,
+    ALU_OR,
+    ALU_AND
+  } alu_op_t;
+
+endpackage
