d37d8997810d8dfbd2af18242d7d19b8629817c5
GBergatto <gb.bergatto@gmail.com>
2026-02-12 21:23:03 +0100

    Make ALU inputs unsigned and add output flags

--- core/rtl/alu.sv
+++ core/rtl/alu.sv
@@ -2,10 +2,13 @@
 import core_types_pkg::*;
 
 module alu (
-    input logic signed [31:0] op1,
-    input logic signed [31:0] op2,
+    input logic [31:0] op1,
+    input logic [31:0] op2,
     input alu_op_t alu_op,
-    output logic [31:0] out
+    output logic [31:0] out,
+    output logic zero,
+    output logic lt,
+    output logic ltu
 );
 
 always_comb begin
@@ -14,14 +17,18 @@ always_comb begin
       ALU_SUB: out = op1 - op2;
       ALU_SLL: out = op1 << op2[4:0];
       ALU_SLT: out = (op1 < op2) ? 1 : 0;
-      ALU_SLTU: out = ($unsigned(op1) < $unsigned(op2)) ? 1 : 0;
+      ALU_SLTU: out = (op1 < op2) ? 1 : 0;
       ALU_XOR: out = op1 ^ op2;
       ALU_SRL: out = op1 >> op2[4:0];
-      ALU_SRA: out = op1 >>> op2[4:0];
+      ALU_SRA: out = $unsigned($signed(op1) >>> op2[4:0]);
       ALU_OR: out = op1 | op2;
       ALU_AND: out = op1 & op2;
       default: out = 0;
    endcase
 end
+
+assign zero = (out == 32'b0);
+assign lt = ($signed(op1) < $signed(op2));
+assign ltu = (op1 < op2);
 
 endmodule
